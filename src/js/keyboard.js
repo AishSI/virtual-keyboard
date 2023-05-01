bb3b7ef22c553bc479948a181bd845156171e66f
AishSI <aishsi@gmail.com>
2023-05-02 03:49:18 +0700

    feat: add save and load language LocalStorage

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -14,8 +14,11 @@ const KEYBOARD = {
   noTextKey: [],
 
   createKeyboardTemplate() {
-    const boardKeys = document.createElement( 'div' );
-    boardKeys.classList.add( 'wrapper' );
+    if (localStorage.getItem('language')) {
+      this.language = localStorage.getItem('language') === 'true';
+    }
+    const boardKeys = document.createElement('div');
+    boardKeys.classList.add('wrapper');
     boardKeys.innerHTML = `
     <h1 class="title">RSS Виртуальная клавиатура</h1>
     <textarea class="textarea" id="textScreen"></textarea>    
@@ -23,39 +26,39 @@ const KEYBOARD = {
     <p class="description">Клавиатура создана в операционной системе Windows</p>
     <p class="description">Переключение языка: левые Ctrl + Alt</p>
     `;
-    BODY.append( boardKeys );
-    this.keyboard = document.querySelector( '.keyboard' );
-    this.textScreen = document.querySelector( '#textScreen' );
+    BODY.append(boardKeys);
+    this.keyboard = document.querySelector('.keyboard');
+    this.textScreen = document.querySelector('#textScreen');
   },
 
   fillKeysKeyboard() {
-    this.layoutKeys = parseInt( ( `${ Number( this.language ) }${ Number( this.caps ) }${ Number( this.shiftLeftActive || this.shiftRightActive ) }` ), 2 );
-    const board = document.querySelector( '.keyboard' );
+    this.layoutKeys = parseInt((`${Number(this.language)}${Number(this.caps)}${Number(this.shiftLeftActive || this.shiftRightActive)}`), 2);
+    const board = document.querySelector('.keyboard');
     board.innerHTML = '';
     const keys = KEYS[this.layoutKeys];
     this.keyCode = [...KEYS[8]];
     this.noTextKey = [...KEYS[10]];
-    keys.forEach( ( element, index ) => {
-      const key = document.createElement( 'button' );
-      key.classList.add( 'keyboard__key' );
-      key.classList.add( `${ this.keyCode[index] }` );
-      switch ( index ) {
+    keys.forEach((element, index) => {
+      const key = document.createElement('button');
+      key.classList.add('keyboard__key');
+      key.classList.add(`${this.keyCode[index]}`);
+      switch (index) {
         case 28:
           key.innerHTML = 'Del';
           break;
         case 29:
-          if ( this.caps ) key.classList.add( 'active' );
+          if (this.caps) key.classList.add('active');
           key.innerHTML = 'CapsLock';
           break;
         case 42:
-          if ( this.shiftLeftActive ) key.classList.add( 'active' );
+          if (this.shiftLeftActive) key.classList.add('active');
           key.innerHTML = 'Shift';
           break;
         case 53:
           key.innerHTML = '&#9650;';
           break;
         case 54:
-          if ( this.shiftRightActive ) key.classList.add( 'active' );
+          if (this.shiftRightActive) key.classList.add('active');
           key.innerHTML = 'Shift';
           break;
         case 55:
@@ -80,50 +83,50 @@ const KEYBOARD = {
           key.innerHTML = 'Ctrl';
           break;
         default:
-          key.innerHTML = `${ element }`;
+          key.innerHTML = `${element}`;
       }
-      board.append( key );
-    } );
+      board.append(key);
+    });
   },
 
   updateKeysKeyboard() {
-    this.layoutKeys = parseInt( ( `${ Number( this.language ) }${ Number( this.caps ) }${ Number( this.shiftLeftActive || this.shiftRightActive ) }` ), 2 );
+    this.layoutKeys = parseInt((`${Number(this.language)}${Number(this.caps)}${Number(this.shiftLeftActive || this.shiftRightActive)}`), 2);
     const keys = KEYS[this.layoutKeys];
-    const keysUpdate = document.querySelectorAll( '.keyboard__key' );
+    const keysUpdate = document.querySelectorAll('.keyboard__key');
     const keysUpdateIndex = [...KEYS[9]];
 
-    keysUpdateIndex.forEach( ( item ) => {
+    keysUpdateIndex.forEach((item) => {
       keysUpdate[item].innerHTML = '';
-      keysUpdate[item].innerHTML = `${ keys[item] }`;
-    } );
+      keysUpdate[item].innerHTML = `${keys[item]}`;
+    });
 
-    if ( this.caps ) keysUpdate[29].classList.add( 'active' );
-    if ( !this.caps ) keysUpdate[29].classList.remove( 'active' );
+    if (this.caps) keysUpdate[29].classList.add('active');
+    if (!this.caps) keysUpdate[29].classList.remove('active');
 
-    if ( this.shiftLeftActive ) keysUpdate[42].classList.add( 'active' );
-    if ( !this.shiftLeftActive ) keysUpdate[42].classList.remove( 'active' );
+    if (this.shiftLeftActive) keysUpdate[42].classList.add('active');
+    if (!this.shiftLeftActive) keysUpdate[42].classList.remove('active');
 
-    if ( this.shiftRightActive ) keysUpdate[54].classList.add( 'active' );
-    if ( !this.shiftRightActive ) keysUpdate[54].classList.remove( 'active' );
+    if (this.shiftRightActive) keysUpdate[54].classList.add('active');
+    if (!this.shiftRightActive) keysUpdate[54].classList.remove('active');
   },
 
   mouseKeyboard() {
-    this.keyboard.addEventListener( 'mousedown', ( event ) => {
-      if ( event.target.classList.contains( 'ShiftLeft' ) ) {
+    this.keyboard.addEventListener('mousedown', (event) => {
+      if (event.target.classList.contains('ShiftLeft')) {
         this.shiftLeftActive = true;
       }
-      if ( event.target.classList.contains( 'ShiftRight' ) ) {
+      if (event.target.classList.contains('ShiftRight')) {
         this.shiftRightActive = true;
       }
-      if ( event.target.innerText === 'CapsLock' ) {
+      if (event.target.innerText === 'CapsLock') {
         this.caps = !this.caps;
       }
-      if ( event.target.innerText === 'CapsLock' || event.target.innerText === 'Shift' ) {
+      if (event.target.innerText === 'CapsLock' || event.target.innerText === 'Shift') {
         this.updateKeysKeyboard();
       }
 
-      if ( event.target.closest( '.keyboard__key' ) ) {
-        switch ( event.target.classList[1] ) {
+      if (event.target.closest('.keyboard__key')) {
+        switch (event.target.classList[1]) {
           // case 'Backspace':
           //   textScreen.value += '';
           //   break;
@@ -140,45 +143,45 @@ const KEYBOARD = {
             this.textScreen.value += ' ';
             break;
           default:
-            if ( !this.noTextKey.includes( event.target.classList[1] ) ) {
+            if (!this.noTextKey.includes(event.target.classList[1])) {
               this.textScreen.value += event.target.innerText;
             }
         }
       }
-    } );
+    });
 
-    this.keyboard.addEventListener( 'mouseup', ( event ) => {
-      if ( event.target.classList.contains( 'ShiftLeft' ) ) {
+    this.keyboard.addEventListener('mouseup', (event) => {
+      if (event.target.classList.contains('ShiftLeft')) {
         this.shiftLeftActive = false;
         this.updateKeysKeyboard();
       }
-      if ( event.target.classList.contains( 'ShiftRight' ) ) {
+      if (event.target.classList.contains('ShiftRight')) {
         this.shiftRightActive = false;
         this.updateKeysKeyboard();
       }
-    } );
+    });
   },
 
   physicalKeys() {
-    document.addEventListener( 'keydown', ( event ) => {
+    document.addEventListener('keydown', (event) => {
       event.preventDefault();
-      if ( this.keyCode.includes( event.code ) ) {
-        if ( event.code === 'CapsLock' ) {
+      if (this.keyCode.includes(event.code)) {
+        if (event.code === 'CapsLock') {
           this.caps = !this.caps;
           this.updateKeysKeyboard();
-        } else if ( event.key === 'Shift' ) {
-          if ( event.code === 'ShiftLeft' ) {
+        } else if (event.key === 'Shift') {
+          if (event.code === 'ShiftLeft') {
             this.shiftLeftActive = true;
           }
-          if ( event.code === 'ShiftRight' ) {
+          if (event.code === 'ShiftRight') {
             this.shiftRightActive = true;
           }
           this.updateKeysKeyboard();
         } else {
-          document.querySelector( `.${ event.code }` ).classList.add( 'active' );
+          document.querySelector(`.${event.code}`).classList.add('active');
         }
 
-        switch ( event.code ) {
+        switch (event.code) {
           case 'Tab':
             this.textScreen.value += '    ';
             break;
@@ -189,47 +192,47 @@ const KEYBOARD = {
             this.textScreen.value += ' ';
             break;
           default:
-            if ( !this.noTextKey.includes( event.code ) ) {
-              const letter = document.querySelector( `.${ event.code }` );
+            if (!this.noTextKey.includes(event.code)) {
+              const letter = document.querySelector(`.${event.code}`);
               this.textScreen.value += letter.innerText;
-            };
-        };
-
+            }
+        }
       }
-    } );
+    });
 
-    document.addEventListener( 'keyup', ( event ) => {
+    document.addEventListener('keyup', (event) => {
       event.preventDefault();
-      if ( this.keyCode.includes( event.code ) ) {
-        if ( event.key === 'Shift' ) {
+      if (this.keyCode.includes(event.code)) {
+        if (event.key === 'Shift') {
           this.shiftLeftActive = false;
           this.shiftRightActive = false;
           this.updateKeysKeyboard();
         }
-        if ( event.code !== 'CapsLock' ) {
-          document.querySelector( `.${ event.code }` ).classList.remove( 'active' );
+        if (event.code !== 'CapsLock') {
+          document.querySelector(`.${event.code}`).classList.remove('active');
         }
       }
-    } );
+    });
   },
 
   runShortcut() {
     const combination = ['AltLeft', 'ControlLeft'];
     const pressed = new Set();
 
-    document.addEventListener( 'keydown', ( event ) => {
+    document.addEventListener('keydown', (event) => {
       event.preventDefault();
-      pressed.add( event.code );
-      if ( combination.every( ( key ) => pressed.has( key ) ) ) {
+      pressed.add(event.code);
+      if (combination.every((key) => pressed.has(key))) {
         pressed.clear();
         this.language = !this.language;
+        localStorage.setItem('language', this.language);
         this.updateKeysKeyboard();
       }
-    } );
+    });
 
-    document.addEventListener( 'keyup', ( event ) => {
-      pressed.delete( event.code );
-    } );
+    document.addEventListener('keyup', (event) => {
+      pressed.delete(event.code);
+    });
   },
 };
 
